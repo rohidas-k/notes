72033c324ca2fa30932944c2489d1bfc519c76fe
rohidasa <rohidasa.rd@gmail.com>
2026-06-27 18:26:46 +0530

    s

---
+++ beachhealinghome_build/assets/LogoutPage-rqJqdxnc.js
@@ -0,0 +1,1 @@
+import{a,b as o,r as s}from"./index-CxFAq7O3.js";function r(){const{logout:e}=a(),t=o();return s.useEffect(()=>{e().then(()=>t("/login",{replace:!0}))},[e,t]),null}export{r as default};
---
+++ beachhealinghome_build/assets/Stars-Bw_kltlL.js
@@ -0,0 +1,11 @@
+import{c as o}from"./SiteLayout-BmTfcm-6.js";import{j as t}from"./index-CxFAq7O3.js";/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M16 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z",key:"rib7q0"}],["path",{d:"M5 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z",key:"1ymkrd"}]],i=o("quote",e);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z",key:"r04s7s"}]],l=o("star",r);function m({rating:s}){return t.jsx("div",{className:"flex gap-0.5",children:Array.from({length:5}).map((c,a)=>t.jsx(l,{className:a<s?"h-4 w-4 fill-gold text-gold":"h-4 w-4 text-sand"},a))})}export{i as Q,m as S};
